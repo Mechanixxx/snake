57fa9ea62e9ad2299f88445b36e2aabdd9ff4387
Igor <57706890+Mechanixxx@users.noreply.github.com>
2020-01-05 13:59:43 +0300

    Методы Move, Clear, наследование, инкапсуляция, приводим змею в движение:)

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -10,7 +11,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            //Console.SetBufferSize( 80, 25 );
+            //Console.SetBufferSize(80, 25);
 
             //Рамка
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
@@ -26,8 +27,27 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
 
             Console.ReadLine();
+            
 
 
 
